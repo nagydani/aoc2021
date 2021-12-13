4eb5ffc57e1ed8f9cbcadceab8e825a1cafde6bf
Daniel A. Nagy <nagy.da@gmail.com>
2021-12-13 08:12:56 +0100

    Solutions, day 13

---
+++ code13b.hs
@@ -0,0 +1,64 @@
+import qualified Data.Set as S
+import Data.List
+import Text.Parsec
+import Text.Parsec
+import Text.Parsec.String (Parser)
+
+type Point = (Int, Int)
+type Edge = (Bool, Int)
+type Page = S.Set Point
+
+point :: Parser Point
+point = do
+    x <- many digit
+    char ','
+    y <- many digit
+    char '\n'
+    return (read x, read y)
+
+edge :: Parser Edge
+edge = do
+    string "fold along "
+    a <- letter
+    char '='
+    v <- many digit
+    char '\n'
+    return (a == 'x', read v)
+
+input :: Parser ([Point], [Edge])
+input = do
+    ps <- many point
+    char '\n'
+    es <- many edge
+    return (ps, es)
+
+readIt :: Either a ([Point], [Edge]) -> (Page, [Edge])
+readIt (Left _) = (S.empty, [])
+readIt (Right (ps, es)) = (foldr S.insert S.empty ps, es)
+
+foldUp :: Edge -> Point -> Point
+foldUp (a, v) (x, y)
+    | a         = (if x > v then v + v - x else x, y)
+    | otherwise = (x, if y > v then v + v - y else y)
+
+foldOnce :: Page -> Edge -> Page
+foldOnce ps e = S.map (foldUp e) ps
+
+foldIt :: Page -> [Edge] -> Page
+foldIt = foldl' foldOnce
+
+showIt :: Page -> String
+showIt p =
+    let ps = S.elems p
+        yMin = minimum $ map fst ps
+        yMax = maximum $ map fst ps
+        xMin = minimum $ map snd ps
+        xMax = maximum $ map snd ps
+        cs = map (\y -> zip [yMin..yMax] (repeat y)) [xMin..xMax]
+        gs = map (map (\c -> if S.member c p then '#' else '.')) cs
+    in unlines gs
+
+main = do
+    f <- readFile "input13.txt"
+    let (ps, es) = readIt $ parse input "" f
+    putStr $ showIt $ foldIt ps es
