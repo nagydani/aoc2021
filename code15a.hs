0ef12e04cec3cd5aa96984d9b40d0dade0032690
Daniel A. Nagy <nagy.da@gmail.com>
2021-12-15 08:32:47 +0100

    speedup

--- code15a.hs
+++ code15a.hs
@@ -7,6 +7,10 @@ import Control.Monad
 
 type Point = (Int, Int)
 data Distance = D Int | Infty deriving (Eq, Ord)
+type DistanceMap = M.Map Point Int
+
+at :: DistanceMap -> Point -> Distance
+at m p = if M.member p m then D (m M.! p) else Infty
 
 dist :: Distance -> Int
 dist (D x) = x
@@ -14,25 +18,25 @@ dist (D x) = x
 dijkstra :: Array Point Int -> Int
 dijkstra a =
     let unvisited = S.fromList $ indices a
-        distances = M.insert (0, 0) (D 0) $ M.fromSet (\_ -> Infty) unvisited
+        distances = M.singleton (0, 0) 0
         current = (0, 0)
     in dijkstra3 a unvisited distances current
 
 addDist :: Array Point Int -> Point -> 
-                M.Map Point Distance -> Point -> M.Map Point Distance
+                DistanceMap -> Point -> DistanceMap
 addDist a current distances p =
-    M.insert p (minimum [distances M.! p,
-                         D (dist (distances M.! current) + a ! p)]) distances
+    M.insert p (dist (minimum [distances `at` p,
+                         D (dist (distances `at` current) + a ! p)])) distances
 
-compareDist :: M.Map Point Distance -> Point -> Point -> Ordering
-compareDist d a b = compare (d M.! a) (d M.! b)
+compareDist :: DistanceMap -> Point -> Point -> Ordering
+compareDist d a b = compare (d `at` a) (d `at` b)
 
-smallestDist :: M.Map Point Distance -> S.Set Point -> Point
-smallestDist distances unvisited =
-    minimumBy (compareDist distances) (S.elems unvisited)
+smallestDist :: DistanceMap -> Point
+smallestDist distances =
+    minimumBy (compareDist distances) (M.keys distances)
 
 dijkstra3 :: Array Point Int ->
-    S.Set Point -> M.Map Point Distance -> Point -> Int
+    S.Set Point -> DistanceMap -> Point -> Int
 dijkstra3 a unvisited distances current =
     let ((_, _), (bottom, right)) = bounds a
         (y, x) = current
@@ -40,9 +44,9 @@ dijkstra3 a unvisited distances current =
         unvisitedNeighbors = filter (\x -> S.member x unvisited) neighbors
         distances' = foldl' (addDist a current) distances unvisitedNeighbors
         unvisited' = S.delete current unvisited
-    in if current == (bottom, right) then dist $ distances' M.! current
-        else dijkstra3 a unvisited' distances' $
-                       smallestDist distances' unvisited'
+        distances'' = M.delete current distances'
+    in if current == (bottom, right) then dist $ distances' `at` current
+        else dijkstra3 a unvisited' distances'' $ smallestDist distances''
 
 main = do
     f <- readFile "input15.txt"
