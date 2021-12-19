0b77682d254b7090b65b120dab297827d0d12174
Daniel A. Nagy <nagy.da@gmail.com>
2021-12-19 09:54:10 +0100

    Solution to part 1, day 19

--- code19a.hs
+++ code19a.hs
@@ -1,9 +1,10 @@
 import Data.List
 import qualified Data.Set as S
 import Control.Monad
+import Text.Parsec
+import Text.Parsec.String (Parser)
 
-shift :: Int -> [a] -> [a]
-shift n xs = drop n xs ++ take n xs
+-- Linear algebra
 
 cross :: [Int] -> [Int] -> [Int]
 [a, b, c] `cross` [d, e, f] =
@@ -19,6 +20,9 @@ scale n = map (*n)
 rot :: [[Int]] -> [Int] -> [Int]
 rot m a = foldl1' add $ zipWith scale a m
 
+shift :: Int -> [a] -> [a]
+shift n xs = drop n xs ++ take n xs
+
 orient :: [[[Int]]]
 orient = do
     c <- [1, -1]
@@ -29,3 +33,72 @@ orient = do
     let y = shift s $ 0 : shift u [d, 0]
     let z = x `cross` y
     return [x, y, z]
+
+-- Parsing
+
+nat :: Parser Int
+nat = do
+    xs <- many1 digit
+    return (read xs)
+
+neg :: Parser Int
+neg = do
+    char '-'
+    n <- nat
+    return (-n)
+
+vec :: Parser [Int]
+vec = do
+    v <- (neg <|> nat) `sepBy1` (char ',')
+    char '\n'
+    return v
+
+scanner :: Parser [[Int]]
+scanner = do
+    string "--- scanner "
+    nat
+    string " ---\n"
+    many vec
+
+puzzle :: Parser [[[Int]]]
+puzzle = scanner `sepBy` (char '\n')
+
+readIt :: Either a [b] -> [b]
+readIt (Left _) = []
+readIt (Right xs) = xs
+
+-- Alignment
+
+align :: S.Set [Int] -> [[Int]] -> [S.Set [Int]]
+align aset bs = do
+    rotation <- orient
+    let rbs = map (rot rotation) bs
+    let as = S.toList aset
+    a <- as
+    b <- rbs
+    let disp = a `sub` b
+    let bset = S.fromList (map (add disp) rbs)
+    let uset = aset `S.union` bset
+    guard $ S.size uset + 12 <= S.size aset + S.size bset
+    return uset
+
+extend :: S.Set [Int] -> [[[Int]]] -> S.Set [Int]
+extend aset bss =
+    let bs = head bss
+        usets = align aset bs
+        uset = if null usets then aset else head usets -- cut
+        tbss = tail bss
+    in if null tbss then uset else extend uset tbss
+
+buildSet :: S.Set [Int] -> [[[Int]]] -> S.Set [Int]
+buildSet aset bss =
+    let uset = extend aset bss
+    in if uset == aset then aset else buildSet uset bss
+
+-- main
+
+main = do
+    f <- readFile "input19.txt"
+    let (p:ps) = readIt $ parse puzzle "" f
+    let uset = buildSet (S.fromList p) ps
+    putStrLn $ show $ S.size uset
