12d30449972bf680970ebdca6d858a1cd19ff1a1
Daniel A. Nagy <nagy.da@gmail.com>
2021-12-20 08:36:56 +0100

    WRONG Solution part 1, day 20

--- code20a.hs
+++ code20a.hs
@@ -3,7 +3,7 @@ import Data.Array
 import Data.List
 
 type Point = (Int, Int)
-type Grid = S.Set Point
+type Grid = (Bool, S.Set Point)
 type Algo = Array (Int) Bool
 
 readInput :: [[Char]] -> Grid
@@ -11,7 +11,10 @@ readInput ls =
     let h = length ls
         w = length $ head ls
         ixs = [ (y, x) | y <- [1..h], x <- [1..w] ]
-    in S.fromList $ map fst $ filter (\x -> snd x == '#') $ zip ixs $ concat ls
+    in (False, S.fromList $ map fst $ filter (\x -> snd x == '#') $ zip ixs $ concat ls)
+
+lit :: Grid -> Point -> Bool
+lit (i, g) p = if i then not $ S.member p g else S.member p g
 
 fromBinary :: [Bool] -> Int
 fromBinary = foldl' (\a d -> a + a + boolToInt d) 0
@@ -22,28 +25,33 @@ neighbors (y, x) =
      [ (a, b) | a <- [pred y .. succ y], b <- [pred x .. succ x] ]
 
 hood :: Grid -> Point -> Int
-hood s (y, x) =
+hood g (y, x) =
     let ps = neighbors (y, x)
-        bs = map ((flip S.member) s) ps
+        bs = map (lit g) ps
     in fromBinary bs 
 
-update :: Algo -> Grid -> Point -> (Grid -> Grid)
-update a g p = if a ! (hood g p)
-    then S.insert p
-    else S.delete p
+update :: Algo -> Grid -> Point -> (S.Set Point -> S.Set Point)
+update a g p =
+    let i = fst g
+    in if a ! (hood g p)
+        then if i then S.delete p else S.insert p
+        else if i then S.insert p else S.delete p
 
-scope :: Grid -> Grid
+scope :: S.Set Point -> S.Set Point
 scope = S.fromList . concatMap neighbors . S.toList
 
 step :: Algo -> Grid -> Grid
-step a g =
+step a (i, g) =
     let s = S.toList $ scope g :: [Point]
-        us = map (update a g) s :: [Grid -> Grid]
-    in (foldl1' (.) us) g
+        us = map (update a (i, g)) s :: [S.Set Point -> S.Set Point]
+    in if a ! 0
+        then (not i, (foldl1' (.) us) g)
+        else (i, (foldl1' (.) us) g)
 
 main = do
     f <- readFile "input20.txt"
     let (l0:l1:ls) = lines f
     let algo = listArray (0,511) $ map (== '#') l0
     let s0 = readInput ls
-    putStrLn $ show $ S.size $ step algo $ step algo s0
+    let s = s0 : map (step algo) s
+    putStrLn $ show $ S.size $ snd $ (s !! 2)
